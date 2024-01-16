f43fd3646170b2c49b81abc6ec0b2b9bacdcb870
woldak <jogu407@gmail.com>
2024-01-16 15:42:32 +0900

    ---------------------------------------ooo

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path'); // path 모듈을 추가
+app.use(express.static(path.join(__dirname, 'public')));
 
 // 루트 경로에 대한 요청 처리
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+    res.sendFile(path.join(__dirname, '/public/html', 'index.html'))
 });
 
 // 서버를 지정한 포트에서 시작
