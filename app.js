d535e0c67c34dabc96fad2dac320688fec7395f4
woldak <jogu407@gmail.com>
2024-01-17 13:18:23 +0900

    .

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require('path'); // path 모듈을 추가
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views'); // views 폴더 경로 설정 (선택적)
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 // 루트 경로에 대한 요청 처리
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/html', 'index.html'))
